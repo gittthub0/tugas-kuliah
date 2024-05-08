72eaa81c2a4b8b7590dac7c858d98fd3dc68a435
gittthub0 <rahmatenomoto687@gmail.com>
2024-05-08 13:11:13 +0700

    add uts

---
+++ modul_8/output/Demo.java
@@ -0,0 +1,11 @@
+package modul_8.output;
+
+import javax.swing.JFrame;
+
+public class Demo {
+    public static void main(String[] args) {
+        JFrame f=new JFrame("demo swing");
+        f.setSize(200,100);
+        f.setVisible(true);
+    }
+}
---
+++ modul_8/program1/App.java
@@ -0,0 +1,13 @@
+package modul_8.program1;
+
+import javax.swing.JFrame;
+
+public class App {
+    public static void main(String[] args) throws Exception {
+        JFrame myWindow = new JFrame();
+        myWindow.setTitle("Aplikasi Kasir");
+        myWindow.setSize(400,200);
+        myWindow.setVisible(true);
+    }
+}
+ 
